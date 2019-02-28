5e38788bdd241ef42cd7e647b2ddc5f46ba23858
heft johnson <46317043+heftjohnson@users.noreply.github.com>
2019-02-28 18:30:12 -0500

    updt

--- Files/ASM Files/rainbow_mario.asm
+++ Files/ASM Files/rainbow_mario.asm
@@ -1,11 +1,11 @@
-ORG $F6F6
-autoclean JSL bro
-NOP
+ORG $F6F6 ; Origin/Original address in ScrollingRoutine, DOESN'T get called outside of levels.
+autoclean JSL rainbow
+NOP ; One leftover byte, do nothing
 
-freecode
+freecode ; Use free space in the RAM for our hijack.
 
-bro:
-INC $19
-LDA $1464
-STA $001C
-RTL
+rainbow: ; Classified on line 2
+INC $19 ; Increases player powerstate by 1 using $7E0019 (shortened to $19)
+LDA $1464 ; Original code to keep scrolling intact.
+STA $001C ; Original code to keep scrolling intact.
+RTL ; Return JSL.
